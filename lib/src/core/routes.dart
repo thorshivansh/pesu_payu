e0a192a6c761d4e56a3cc745c2204356995af019
Shivanshu <shivanshpal048@gmail.com>
2024-07-29 14:31:24 +0530

    routes1

--- lib/src/core/routes.dart
+++ lib/src/core/routes.dart
@@ -4,7 +4,7 @@ class PaymentRoutes{
 static Future<void> preventDuplicate(dynamic pageName , BuildContext context)async{
    Navigator.popUntil(context, ModalRoute.withName(pageName));
     //  Get.toNamed(pageName);
-    Navigator.popAndPushNamed(context, pageName);
+    Navigator.pushNamed(context, pageName);
 
 }
 }
